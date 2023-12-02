84648f285964099641a4834833cc8e2f00d3c8ea
gracieliuhoo <146503607+gracieliuhoo@users.noreply.github.com>
2023-12-02 03:52:18 +0000

    Fri, Dec 1, 2023, 10:52 PM -05:00

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-
-
 document.querySelector('#rating1').addEventListener('click', function() {
     // update rating
 });
@@ -8,3 +6,11 @@ document.querySelector('#submit').addEventListener('click', function() {
     // get text from textarea
     var review = document.querySelector('#review').value;
 });
+
+// For each radio button...
+    // Add an event listener that runs a function when the button is clicked
+
+// In the function...
+    // For each radio button...
+        // If the button's value is less than or equal to the clicked button's value...
+            // Change the appearance of the button to make it look selected
