383304e9e03bc7bafd1cda8c81a0d139b2f376c3
gracieliuhoo <146503607+gracieliuhoo@users.noreply.github.com>
2023-12-02 05:37:20 +0000

    Sat, Dec 2, 2023, 12:37 AM -05:00

--- app.py
+++ app.py
@@ -184,17 +184,19 @@ def logout():
 @app.route("/")
 @login_required
 def home():
-    user_id = session["user_id"]
-    # get access to the food items, date they are serverd, mealtime, where they are served and nutrition facts in a database
+    if request.method == "GET":
+        user_id = session["user_id"]
+        # get access to the food items, date they are serverd, mealtime, where they are served and nutrition facts in a database
 
-    # first we need to be able to loop through the API and generate a list of say, the menu items
-    # to do this we need to index through everything, specify what we want, and return all those values in a single list
-    # then export the data to food.db
-    #recipe_names = []
-    #for item in data:
-    #    recipe_names.append(item['Recipe_Name'])
-    #return recipe_names
+        # first we need to be able to loop through the API and generate a list of say, the menu items
+        # to do this we need to index through everything, specify what we want, and return all those values in a single list
+        # then export the data to food.db
+        #recipe_names = []
+        #for item in data:
+        #    recipe_names.append(item['Recipe_Name'])
+        #return recipe_names
 
+        return render_template("home.html")
     return render_template("home.html")
 
 
