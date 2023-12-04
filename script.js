46dc65bf38777171d7668083de00b7021d69d2be
gracieliuhoo <146503607+gracieliuhoo@users.noreply.github.com>
2023-12-04 04:19:03 +0000

    Sun, Dec 3, 2023, 11:19 PM -05:00

--- script.js
+++ script.js
@@ -68,3 +68,13 @@ console.log('Review Submitted:', review);
         selectedRating = rating;
         document.getElementById('selectedRating').innerText = `Selected Rating: ${rating} stars`;
         }
+/* redirect user to home page after submit */
+
+        function redirectToHome() {
+            // Perform any form processing here if needed
+            // e.g., sending data to a server using AJAX
+
+            // Redirect to home.html
+            window.location.href = "home.html";
+            return false; // Prevent the form from submitting in the traditional way
+        }
