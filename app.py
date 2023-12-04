a25e7ed882bab8cdfa5c834db7809de05c99b08e
alexli2800 <144275606+alexli2800@users.noreply.github.com>
2023-12-04 04:15:46 +0000

    Sun, Dec 3, 2023, 11:15 PM -05:00

--- app.py
+++ app.py
@@ -393,8 +393,10 @@ def rating():
     if request.method=="GET":
         # when you rate the item, the form you use in the html gives you a number from 0 to 5 and you send that to the rating database
         rating = request.form.get("rating")
+        review = request.form.get("review")
         if not rating:
             return apology("Missing Submission", 400)
+
         return render_template("rating.html")
     else:
         return redirect("/")
