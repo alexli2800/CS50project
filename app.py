f21fbeaec70575d0c32ace0c09f5b9c76f66f611
gracieliuhoo <146503607+gracieliuhoo@users.noreply.github.com>
2023-12-06 21:37:59 +0000

    Wed, Dec 6, 2023, 4:37 PM -05:00

--- app.py
+++ app.py
@@ -450,10 +450,9 @@ def dinner():
 @app.route("/submit_review", methods=["POST"])
 def submit_review():
     # Handle the form submission logic here
-    # For example, you can process the form data, store it in the database, etc.
 
-    # Redirect to the home page after processing the form
-    return redirect(url_for("home"))  # Assuming you have a route named "home"
+    # Redirect to home page after processing the form
+    return redirect(url_for("home"))
 
 
 
