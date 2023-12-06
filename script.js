98e3eeffe24032cbbf19440a4889d87775aeaa57
gracieliuhoo <146503607+gracieliuhoo@users.noreply.github.com>
2023-12-06 20:40:36 +0000

    Wed, Dec 6, 2023, 3:40 PM -05:00

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var starInputs = document.querySelectorAll('.rate input');
+    for (var i = 0; i < starInputs.length; i++) {
+        starInputs[i].addEventListener('click', function(event) {
+            event.preventDefault(); // This will prevent the default action of the input which is to jump to the top of the page
+        });
+    }
+});
 
 $(':radio').change(function() {
 console.log('New star rating: ' + this.value);
