9e628539456d584999276da2a8460ff03bb724d3
alexli2800 <144275606+alexli2800@users.noreply.github.com>
2023-12-07 05:26:43 +0000

    Thu, Dec 7, 2023, 12:26 AM -05:00

--- app.py
+++ app.py
@@ -231,12 +231,13 @@ def rating():
 @login_required
 def lunch():
     user_id = session["user_id"]
+    formatted_date = datetime.now().strftime('%m/%d/%Y')
     if request.method == "POST":
         # define formatted_date
         rating = request.form.get("rating")
         review = request.form.get("review")
         db.execute("INSERT INTO Ratings (user_id, date, rating, review) VALUES (?, ?, ?, ?)", user_id, formatted_date, rating, review)
-        formatted_date = datetime.now().strftime('%m/%d/%Y')
+
         lunch_entree = db.execute("""
                             SELECT DISTINCT recipe_name FROM Meal
                             WHERE (location_name LIKE '%Adams%'
