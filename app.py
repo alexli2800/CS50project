d18449b9f2081d9d1f25295b2c94997f67a0a355
alexli2800 <144275606+alexli2800@users.noreply.github.com>
2023-12-07 06:25:55 +0000

    Thu, Dec 7, 2023, 1:25 AM -05:00

--- app.py
+++ app.py
@@ -239,7 +239,7 @@ def lunch():
             return apology("Missing Rating", 400)
         if not review:
             return apology("Missing Review", 400)
-        db.execute("INSERT INTO Ratings (user_id, date, rating, review) VALUES (?, ?, ?, ?)", user_id, formatted_date, rating, review)
+        db.execute("INSERT INTO Ratings (user_id, date, rating, review, meal_time) VALUES (?, ?, ?, ?, lunch)", user_id, formatted_date, rating, review)
     if request.method == "GET":
         # define formatted_date
         lunch_entree = db.execute("""
