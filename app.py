a332e2b598e088901adb54fecf7cf954a92e2ed2
alexli2800 <144275606+alexli2800@users.noreply.github.com>
2023-12-08 02:51:46 +0000

    Thu, Dec 7, 2023, 9:51 PM -05:00

--- app.py
+++ app.py
@@ -204,31 +204,31 @@ def generate_data():
         db.execute("DELETE FROM Meal")
         formatted_date = datetime.now().strftime("%m/%d/%Y")
 
-        valid_locations = [
-            '%Adams%',
-            '%Lowell%',
-            '%Quincy%',
-            '%Leverett%',
-            '%Mather%',
-            '%Dunster%',
-            '%Eliot%',
-            '%Kirkland%',
-            '%Winthrop%',
-            '%Cabot%',
-            '%Pforzheimer%',
-            '%Currier%',
-            '%Annenberg%'
-        ]
+        # valid_locations = [
+        #     '%Adams%',
+        #     '%Lowell%',
+        #     '%Quincy%',
+        #     '%Leverett%',
+        #     '%Mather%',
+        #     '%Dunster%',
+        #     '%Eliot%',
+        #     '%Kirkland%',
+        #     '%Winthrop%',
+        #     '%Cabot%',
+        #     '%Pforzheimer%',
+        #     '%Currier%',
+        #     '%Annenberg%'
+        # ]
 
         for item in data:
             serve_date = item.get("Serve_Date", "")
-            location_name = item.get("Location_Name", "")
-            if (serve_date == formatted_date and any(location in location_name for location in valid_locations)):
+            # location_name = item.get("Location_Name", "")
+            if (serve_date == formatted_date):
                 db.execute(
                     "INSERT INTO Meal (date, meal_time, location_name, recipe_name, meal_category) VALUES (?, ?, ?, ?, ?)",
                     formatted_date,
                     item["Meal_Name"],
-                    location_name,
+                    item["Location_Name"],
                     item["Recipe_Print_As_Name"],
                     item["Menu_Category_Name"],
                 )
