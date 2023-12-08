92aac19d0f9a55d9e345f51b7b80145763dd1c45
gracieliuhoo <146503607+gracieliuhoo@users.noreply.github.com>
2023-12-08 04:27:20 +0000

    Thu, Dec 7, 2023, 11:27 PM -05:00

--- static/script.js
+++ static/script.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', function() {
     var starInputs = document.querySelectorAll('.rate input');
     for (var i = 0; i < starInputs.length; i++) {
         starInputs[i].addEventListener('click', function(event.preventDefault()) {
-            event.preventDefault(); // This will prevent the default action of the input which is to jump to the top of the page
+            event.preventDefault(); // This will prevent jump to the top of the page
         });
     }
     return false;
@@ -12,15 +12,13 @@ document.addEventListener('DOMContentLoaded', function() {
 /* make the submit button redirect home */
 function redirectToHome() {
     console.log("Redirecting to home");
-    // Add any additional logic or validation here if needed
 
     // Redirect to the home page
-    window.location.href = "/home.html"; // Replace "/" with the actual URL of your home page
-    return false; // Prevent the form from submitting (if needed)
+    window.location.href = "/home.html";
+    return false;
 }
 
 /* for star rating */
 $(':radio').change(function() {
-console.log('New star rating: ' + this.value);
+    console.log('New star rating: ' + this.value);
 });
-
